105269b7ef4491843e6ea75cb2840dc16a01968d
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-11-02 11:53:09 +0000

    Deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  
+  experimental: {
+    optimizeCss: false, // ✅ disables lightningcss
+  },
 };
 
 export default nextConfig;
